ec275a0c2c3e303f053cba89c910b6952ab4dddf
Adam Barron <adambarron@me.com>
2021-02-27 17:40:41 -0800

    updated all queries and mutations (may need to make adjustments when integrating with front end)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,37 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World!';
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
         }
     },
     Mutation: {
-        addUser: async () => {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
 
+            return user;
         },
-        login: async () => {
-            
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return user;
         }
     }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,42 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    input BookInput {
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
     type Query {
-        helloWorld: String
+        me: User
+        users: [User]
+        user(username: String!): User
     }
     type Auth {
         token: ID!
         user: User
     }
     type Mutation {
-        login(email: String!, password: String!): User
-        addUser(username: String!, email: String!, password: String!): User
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: String): User
     }
 `;
 
